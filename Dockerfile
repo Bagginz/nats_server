5770ef31d71ac3215d60356ff9b50fefbb016329
Pariwat <pariwat@theiconweb.com>
2019-05-25 06:26:35 +0700

    Fix Docker File and run script

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,1 @@
 FROM nats:latest
-
-CMD [ "bash" ]
-
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker rm -f itopplus_nats
-docker run -p 4222:4222 -p 8222:8222 -p 6222:6222 --name itopplus_nats -ti itopplus_nats
+docker run -d -p 4222:4222 -p 8222:8222 -p 6222:6222 --name itopplus_nats -ti itopplus_nats
